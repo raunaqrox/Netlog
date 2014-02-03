8bdb57b5b71729be79f9c7ab4c25dc0d2469c51c
raunaqrox <raunaqrox@yahoo.co.in>
2014-02-04 00:20:46 +0530

    Small bug, little design change.

--- netlog.cpp
+++ netlog.cpp
@@ -10,7 +10,7 @@ int main () {
   time_t t = time(0);   // get time now
     struct tm * now = localtime( & t );
 cout<<"\t\t/*NETLOG*/\n\n";
-myfile<<now->tm_hour<<":"<<now->tm_min<<":"<<now->tm_sec<<"\t"<<now->tm_mday<<":"<<now->tm_mon+1<<":"<<now->tm_year+1900<<endl;
+myfile<<endl<<now->tm_hour<<":"<<now->tm_min<<":"<<now->tm_sec<<"\t"<<now->tm_mday<<":"<<now->tm_mon+1<<":"<<now->tm_year+1900<<endl;
 cout<<"\n#####   #####Enter Your NetLog#####   #####\n\n "<<endl;
 string s;
   getline(cin,s);
